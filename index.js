afd5e52d5fd2b5bb5184df912dd82e245ddd3f93
as111005 <132070654+as111005@users.noreply.github.com>
2025-11-27 16:25:50 +0000

    Added in the training plan query

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 /* Refer to the README.md for instructions on what you need to do in this project */
 
 alert("Hello, World!");
+const trainingPlan = document.querySelector("#training-plan").value;
+let planCosts = {
+beginner: 25 * 4,
+intermediate: 30 * 4,
+elite: 35 * 4,
+};
+let monthlyCost = planCosts[trainingPlan]
