319b7146364895793342f454b8ca3540889203b7
as111005 <132070654+as111005@users.noreply.github.com>
2025-12-04 14:23:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /* Refer to the README.md for instructions on what you need to do in this project */
 
-alert("Hello, World!");
+alert("North Sussex Judo!");
 const trainingPlan = document.querySelector("#training-plan").value;
 let planCosts = {
-beginner: 25 * 4,
-intermediate: 30 * 4,
-elite: 35 * 4,
+  beginner: 25 * 4,
+  intermediate: 30 * 4,
+  elite: 35 * 4,
 };
-let monthlyCost = planCosts[trainingPlan]
+let monthlyCost = planCosts[trainingPlan];
